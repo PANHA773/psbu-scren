1708ff501a607b20d0824a094703a590159c7bcd
PANHA773 <147047545+PANHA773@users.noreply.github.com>
2026-06-23 16:28:11 +0700

    update title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PSBU University</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <title>preah sihamoniraja buddhist university</title>
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com"> -->
+    <link rel="icon" type="image/png" href="images/logo.png">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Noto+Serif+Khmer:wght@400;600;700&family=Cormorant+Garamond:ital,wght@0,400;0,600;1,400&family=DM+Sans:wght@300;400;500;600&display=swap"
