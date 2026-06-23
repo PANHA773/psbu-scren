fee32b29cb433a1d424b90afdd60312d62bb8d5d
PANHA773 <147047545+PANHA773@users.noreply.github.com>
2026-06-23 16:34:52 +0700

    update title 1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>preah sihamoniraja buddhist university</title>
+    <title>Preah Sihamoniraja Buddhist University</title>
     <!-- <link rel="preconnect" href="https://fonts.googleapis.com"> -->
     <link rel="icon" type="image/png" href="images/logo.png">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
